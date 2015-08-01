d992cbefff7ea2ed477de86c75c9aae591503452
Eric Peterson <epeterson@tableausoftware.com>
2015-08-01 10:30:47 -0700

    Reorganizing the _main.js template for sanity, code style.

--- generators/app/templates/_main.js
+++ generators/app/templates/_main.js
@@ -1,25 +1,4 @@
-
-(function() {
-  //
-  // Helper functions
-  //
-
-  // build the spreadsheet url for the given doc key
-  function buildConnectionUrl(docKey) {
-    docKey = docKey.trim();
-    if (stringStartsWith(docKey, "http")) {
-      var urlParts = docKey.split("/");
-      docKey = urlParts[5];
-    }
-    return 'http://spreadsheets.google.com/feeds/cells/' + docKey + '/default/public/values?alt=json';
-  }
-
-  // string.startsWith is not available in all browsers, so use this implementation
-  function stringStartsWith(str, searchString, position) {
-    position = position || 0;
-    return str.indexOf(searchString, position) === position;
-  };
-
+(function($, tableau) {
   var myConnector = tableau.makeConnector();
 
   myConnector.init = function () {
@@ -33,22 +12,30 @@
 
   myConnector.getColumnHeaders = function() {
     _retrieveJsonData(function (tableData) {
-      var fieldNames = [];
-      var fieldTypes = [];
-      var numTypesFound = 0;
-      for (var ii = 0; ii < tableData.feed.entry.length; ++ii) {
-        var entry = tableData.feed.entry[ii];
+      var fieldNames = [],
+          fieldTypes = [],
+          numTypesFound = 0,
+          ii = 0,
+          column,
+          entry,
+          fieldType,
+          fieldVal;
+
+      for (0; ii < tableData.feed.entry.length; ++ii) {
+        entry = tableData.feed.entry[ii];
+
         if (entry.gs$cell.row == "1") {
           // set field names
           fieldNames.push(entry.content.$t);
-        } else {
+        }
+        else {
           // use the first value for a column to guess the type
-          var column = parseInt(entry.gs$cell.col) - 1;
+          column = parseInt(entry.gs$cell.col) - 1;
           // check if we already figured out the type for this column
           if (fieldTypes[column]) continue;
           // try to determine the column type based on this value.
-          var fieldType = 'string';
-          var fieldVal = entry.content.$t;
+          fieldType = 'string';
+          fieldVal = entry.content.$t;
           if (parseInt(fieldVal).toString() == fieldVal) fieldType = 'int';
           else if (parseFloat(fieldVal).toString() == fieldVal) fieldType = 'float';
           else if (isFinite(new Date(fieldVal).getTime())) fieldType = 'date';
@@ -68,12 +55,17 @@
 
   myConnector.getTableData = function (lastRecordToken) {
     _retrieveJsonData(function (tableData) {
-      var toRet = [];
-      var lastRow = "1";
-      var rowData;
-      for (var ii = 0; ii < tableData.feed.entry.length; ++ii) {
-        var entry = tableData.feed.entry[ii];
-        var curRow = entry.gs$cell.row;
+      var toRet = [],
+          lastRow = "1",
+          rowData,
+          ii,
+          entry,
+          curRow,
+          column;
+
+      for (ii = 0; ii < tableData.feed.entry.length; ++ii) {
+        entry = tableData.feed.entry[ii];
+        curRow = entry.gs$cell.row;
         // skip the first row of data.
         if (curRow == "1") continue;
         if (curRow != lastRow)
@@ -86,15 +78,38 @@
           // create an array of empty values.
           rowData = Array(myConnector._numCols).join(".").split(".");;
         }
-        var column = parseInt(entry.gs$cell.col) - 1;
+        column = parseInt(entry.gs$cell.col) - 1;
         rowData[column] = entry.content.$t;
       }
       toRet.push(rowData);
       tableau.dataCallback(toRet, null, false);
     });
-  }
+  };
 
   tableau.registerConnector(myConnector);
+
+
+  //
+  // Helper functions
+  //
+
+  // build the spreadsheet url for the given doc key
+  function buildConnectionUrl(docKey) {
+    var urlParts;
+
+    docKey = docKey.trim();
+    if (stringStartsWith(docKey, "http")) {
+      urlParts = docKey.split("/");
+      docKey = urlParts[5];
+    }
+    return 'http://spreadsheets.google.com/feeds/cells/' + docKey + '/default/public/values?alt=json';
+  }
+
+  // string.startsWith is not available in all browsers, so use this implementation
+  function stringStartsWith(str, searchString, position) {
+    position = position || 0;
+    return str.indexOf(searchString, position) === position;
+  }
 
   // Fetch the spreadsheet data, cache it, and call the given callback function.
   // If the data is already cached, then just call the callback and return
@@ -103,41 +118,44 @@
       retrieveDataCallback(tableau.dataCache);
       return;
     }
-    var docKey = tableau.connectionData;
-    var connectionUrl = buildConnectionUrl(docKey);
+
+    var docKey = tableau.connectionData,
+        connectionUrl = buildConnectionUrl(docKey),
+        msg;
+
     $.ajax({
       url: connectionUrl,
       dataType: 'json',
-      success: function(res, status, xhr)
-      {
+      success: function(res, status, xhr) {
         tableau.dataCache = res;
         retrieveDataCallback(tableau.dataCache);
       },
       error: function (xhr, ajaxOptions, thrownError) {
-        var msg = "error connecting to Google Spreadsheets: " + xhr.responseText + "\n" + thrownError;
+        msg = "error connecting to Google Spreadsheets: " + xhr.responseText + "\n" + thrownError;
         tableau.abortWithError(msg);
       }
     });
   }
-})();
 
-$(document).ready(function(){
-  $("#inputForm").submit(function(evt) { // This event fires when a button is clicked
-    evt.preventDefault();
-    var textField = $('input[type=text]');
-    if (!textField || textField.length == 0)
-    {
-      return;
-    }
-    var inputUrl = textField[0].value,
-      paramName = 'key';
+  $(document).ready(function(){
+    $("#inputForm").submit(function(e) { // This event fires when a button is clicked
+      var textField = $('input[type=text]'),
+          inputUrl = textField[0].value,
+          paramName = 'key',
+          regex = new RegExp("[\\?&]" + paramName + "=([^&#]*)"),
+          results = regex.exec(inputUrl),
+          docKey = results == null ? inputUrl : decodeURIComponent(results[1].replace(/\+/g, " "));
 
-    var regex = new RegExp("[\\?&]" + paramName + "=([^&#]*)"),
-      results = regex.exec(inputUrl),
-      docKey = results == null ? inputUrl : decodeURIComponent(results[1].replace(/\+/g, " "));
+      e.preventDefault();
 
-    tableau.connectionData = docKey;
-    tableau.connectionName = 'Google spreadsheet';
-    tableau.submit();
+      if (!textField || textField.length == 0) {
+        return;
+      }
+
+      tableau.connectionData = docKey;
+      tableau.connectionName = '<%= props.name %>';
+      tableau.submit();
+    });
   });
-});
+
+})(jQuery, tableau);
